ca9eff7066cfb661788ac5f3213e24db9a43bfe9
Viraj Parmar <viraj.parmar@student.tdsb.on.ca>
2024-12-10 13:54:06 -0500

    Initialized fire base

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCcqj_AT_wynCqA8Y_fLfk_EqYuUNpNyfE",
+    authDomain: "summative-48eef.firebaseapp.com",
+    projectId: "summative-48eef",
+    storageBucket: "summative-48eef.firebasestorage.app",
+    messagingSenderId: "454062771770",
+    appId: "1:454062771770:web:8ed15204bdd7aa1584af03"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
