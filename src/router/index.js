f86123f49e079362cc88202930fa67834e57cf69
Viraj Parmar <viraj.parmar@student.tdsb.on.ca>
2025-01-17 14:46:21 -0500

    Added route gaurds

--- src/router/index.js
+++ src/router/index.js
@@ -6,20 +6,36 @@ import MoviesView from '../views/MoviesView.vue';
 import DetailsView from '../views/DetailsView.vue';
 import CartView from '../views/CartView.vue';
 import SettingsView from '../views/SettingsView.vue';
+import ErrorView from '../views/ErrorView.vue';
+import { userAuthorized, useStore } from '../stores';
+
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/login', component: LoginView },
-  { path: '/register', component: RegisterView },
-  { path: '/movies', component: MoviesView },
-  { path: '/movies/:id', component: DetailsView },
-  { path: '/cart', component: CartView },
-  { path: '/settings', component: SettingsView },
-]
+  { path: '/',  meta: { auth: false }, component: HomeView },
+  { path: '/register', meta: { auth: false }, component: RegisterView },
+  { path: '/login', meta: { auth: false }, component: LoginView },
+  { path: '/movies', meta: { auth: true }, component: MoviesView },
+  { path: '/movies/:id', meta: { auth: true }, component: DetailsView },
+  { path: '/cart', meta: { auth: true }, component: CartView },
+  { path: '/settings', meta: { auth: true }, component: SettingsView },
+  { path: '/:pathMatch(.*)*', meta: { auth: false }, component: ErrorView, },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
+
+router.beforeEach((to, from, next) => {
+  userAuthorized.then(() => {
+      const store = useStore();
+
+      if (!store.user && to.meta.auth) {
+          next("/login");
+      } else {
+          next();
+      }
+  });
+});
 
 export default router;
